7567507e9acf109b51adfe83b5b5da60a6252147
Robert Medina <robertm@avid-tech.com>
2019-07-16 15:49:23 -0400

    Update the project to make use of the RX for the UART and to send RX data to the other UART. BLE UART RX ==> Debug UART TX Debug UART RX ==> BLE UART TX

--- BLE_Test_Project/main.c
+++ BLE_Test_Project/main.c
@@ -1,22 +1,6 @@
 ﻿/* Copyright (c) Microsoft Corporation. All rights reserved.
    Licensed under the MIT License. */
 
-   /************************************************************************************************
-   Name:		BLE_Test_Project
-   Sphere OS:	19.02
-   This file contains the 'main' function. Program execution begins and ends there
-
-   Author:
-   Peter Fenn (Avnet Engineering & Technology)
-
-   Purpose:
-   Sphere Starter Kit BLE test project
-
-   Description:
-   ...
-
-   *************************************************************************************************/
-
 #include <errno.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -37,12 +21,22 @@
 #include "epoll_timerfd_utilities.h"
 
 #include <applibs/log.h>
+/* This is here due to the documenation from microsoft 
+ * See URL https://docs.microsoft.com/en-us/azure-sphere/app-development/uart#feedback
+ */
+//#define UART_STRUCTS_VERSION 1
 #include <applibs/uart.h>
 #include <applibs/networking.h>
 
-/* #include "mt3620_rdb.h" */
+/* #include "mt3620_rdb.h */
 #include "mt3620_avnet_dev.h"
 #include "mt3620_rdb.h"
+
+/* Uncomment the below define to enable debugging of the debug UART */
+//#define DEBUG_DEBUG_UART
+
+#define ERROR_RET		( -1 )
+#define SUCCESS_RET		( 0 )
 
 //--------- Support for LPS22HH added ---------//
 
@@ -50,6 +44,8 @@
 static int InitPeripheralsAndHandlers(void);
 static void ClosePeripheralsAndHandlers(void);
 static void SendUartMessage(int uartFd, const char *dataToSend);
+static void uartEventDebugData(EventData *eventData);
+static void uartEventBleData(EventData *eventData);
 
 static char str[512];
 
@@ -68,15 +64,67 @@ static void TerminationHandler(int signalNumber)
 }
 
 /// <summary>
+///     Handle UART event: if there is incoming data, print it, and blink the LED.
+/// </summary>
+static void uartEventDebugData(EventData *eventData)
+{
+	const size_t receiveBufferSize = 256;
+	uint8_t receiveBuffer[receiveBufferSize + 1]; // allow extra byte for string termination
+	ssize_t bytesRead;
+
+	// Read UART message
+	bytesRead = read(uartFd, receiveBuffer, receiveBufferSize);
+	if (bytesRead < 0) {
+		Log_Debug("ERROR: Could not read UART: %s (%d).\n", strerror(errno), errno);
+		terminationRequired = true;
+		return;
+	}
+
+	if (bytesRead > 0) {
+		// Null terminate the buffer to make it a valid string, and print it
+		receiveBuffer[bytesRead] = 0;
+
+		/* Pass the data to the debug port */
+		SendUartMessage(uartFd, ( char* )receiveBuffer);
+	}
+}
+
+/// <summary>
+///     Handle UART event: if there is incoming data, print it, and blink the LED.
+/// </summary>
+static void uartEventBleData(EventData *eventData)
+{
+	const size_t receiveBufferSize = 256;
+	uint8_t receiveBuffer[receiveBufferSize + 1]; // allow extra byte for string termination
+	ssize_t bytesRead;
+
+	// Read UART message
+	bytesRead = read(uartFd, receiveBuffer, receiveBufferSize);
+	if (bytesRead < 0) {
+		Log_Debug("ERROR: Could not read UART: %s (%d).\n", strerror(errno), errno);
+		terminationRequired = true;
+		return;
+	}
+
+	if (bytesRead > 0) {
+		// Null terminate the buffer to make it a valid string, and print it
+		receiveBuffer[bytesRead] = 0;
+
+		/* Pass the data to the Nordic */
+		SendUartMessage(uartBleFd, (char*)receiveBuffer);
+	}
+}
+
+/// <summary>
 ///     Set up SIGTERM termination handler, initialize peripherals, and set up event handlers.
 /// </summary>
 /// <returns>0 on success, or -1 on failure</returns>
 static int InitPeripheralsAndHandlers(void)
 {
-    struct sigaction action;
-    memset(&action, 0, sizeof(struct sigaction));
-    action.sa_handler = TerminationHandler;
-    sigaction(SIGTERM, &action, NULL);
+	struct sigaction action;
+	memset(&action, 0, sizeof(struct sigaction));
+	action.sa_handler = TerminationHandler;
+	sigaction(SIGTERM, &action, NULL);
 
 	// Create a UART_Config object, open the UART and set up UART event handler
 	UART_Config uartConfig;
@@ -84,17 +132,52 @@ static int InitPeripheralsAndHandlers(void)
 	uartConfig.baudRate = 115200;
 	uartConfig.flowControl = UART_FlowControl_None;
 	uartConfig.dataBits = UART_DataBits_Eight;
+	
+	UART_Config uartConfig1;
+	UART_InitConfig(&uartConfig1);
+	uartConfig1.baudRate = 115200;
+	uartConfig1.dataBits = UART_DataBits_Eight;
+	uartConfig1.flowControl = UART_FlowControl_None;
 
 	uartFd = UART_Open(MT3620_UART_ISU0, &uartConfig);
-	uartConfig.flowControl = UART_FlowControl_RTSCTS;
-	uartFd = UART_Open(MT3620_UART_ISU1, &uartConfig);
-	SendUartMessage(uartFd, "Uart is up\n");
+	if (uartFd > 0)
+	{
+		SendUartMessage(uartFd, "Uart is up\n");
+	}
+	else
+	{
+		Log_Debug("Failed to bring up the debug UART\n");
+		return ERROR_RET;
+	}
+
+	if (RegisterEventHandlerToEpoll(epollFd, uartFd, &uartEventDebugData, EPOLLIN) != 0) {
+		return ERROR_RET;
+	}
+
+	//Enable the HW Flow control for the BLE module
+	
+	uartBleFd = UART_Open(MT3620_UART_ISU1, &uartConfig1);
+	if (uartBleFd > 0)
+	{
+		SendUartMessage(uartFd, "BLE Uart is up\n");
+	}
+	else
+	{
+		SendUartMessage(uartFd, "Failed to bring up the BLE UART\n");
+		return ERROR_RET;
+	}
+
+	if (RegisterEventHandlerToEpoll(epollFd, uartBleFd, &uartEventBleData, EPOLLIN) != 0) {
+		return ERROR_RET;
+	}
 
     epollFd = CreateEpollFd();
-    if (epollFd < 0) {
-        return -1;
+    if (epollFd < 0) 
+	{
+		SendUartMessage(uartFd, "Failed to bring up the epollFD\n");
+        return ERROR_RET;
     }
-    return 0;
+    return SUCCESS_RET;
 }
 
 /// <summary>
@@ -123,8 +206,10 @@ static void SendUartMessage(int uartFd, const char *dataToSend)
 		}
 		totalBytesSent += (size_t)bytesSent;
 	}
+#if defined(DEBUG_DEBUG_UART)
 	snprintf(str, sizeof(str), "Sent %zu bytes over UART in %d calls.\n", totalBytesSent, sendIterations );
 	Log_Debug(str);
+#endif
 }
 
 /// <summary>
@@ -160,5 +245,5 @@ int main(int argc, char *argv[])
 
 	Log_Debug("Application exiting.\n");
     ClosePeripheralsAndHandlers();
-    return 0;
+    return SUCCESS_RET;
 }
